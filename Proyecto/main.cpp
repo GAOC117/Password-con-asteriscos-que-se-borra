3ff35b6c9e56243d7ee0eb4532acd1a1f508e039
DJoser <ramonnavarropadilla@hotmail.com>
2015-02-17 13:12:45 -0600

    Agregar Solucion al proyecto

---
+++ Proyecto/main.cpp
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int	main()
+{
+	char cLetter = '0';
+	char pass[11];
+	int iCount = 0;
+	cout << "Introduce a Password" << endl;
+	while (cLetter != 13){
+		cLetter = _getch();
+		if (cLetter > 32 && iCount < 10){
+			pass[iCount] = cLetter;
+			cout << "*";
+			iCount++;
+		}
+		else
+			if (cLetter == 8 && iCount > 0){
+			putchar(8);
+			putchar(' ');
+			putchar(8);
+			iCount--;
+			}
+
+	}
+	pass[iCount] = '\0';
+	if (strcmp(pass, "CTN0452-9") == 0)
+
+		cout << "\nBienvenido!";
+	else
+		cout << "\nLargo Intruzo!";
+
+
+	_getch();
+	return EXIT_SUCCESS;
+}
